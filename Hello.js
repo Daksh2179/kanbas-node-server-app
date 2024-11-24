836e7355d26920f761a7f1af8528b4ceb30de97a
Daksh <daksh2179@gmail.com>
2024-11-24 17:15:55 -0500

    a5

--- Hello.js
+++ Hello.js
@@ -1,8 +1,8 @@
 export default function Hello(app) {
-    app.get('/hello', (req, res) => {
+    app.get('/hello', (_req, res) => {
         res.send('Life is good!')
       })
-      app.get('/', (req, res) => {
+      app.get('/', (_req, res) => {
         res.send('Welcome to Full Stack Development!')
       })   
 }
